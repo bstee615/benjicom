7fbce6ba5094c87cf06aa89871a13f718c2be3ab
bstee615 <benjaminjsteenhoek@gmail.com>
2019-05-28 09:15:19 -0500

    Removed old project source. Restarting from Vue CLI.

--- scripts/install_reqs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo yum -y update
-sudo yum install -y git node yarn
-cd /home/ec2-user
-git clone https://github.com/bstee615/benjicom.git
-cd benjicom
-sudo bash scripts/install_yarn.sh
-sudo yarn install
