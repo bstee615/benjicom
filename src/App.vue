c53508323cd82ccc9f36f1ca258055647e9128ef
bstee615 <benjaminjsteenhoek@gmail.com>
2019-05-28 10:34:51 -0500

    Shove around stuff to restore original site.

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,75 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Header />
+
+    <hr />
+
     <router-view />
   </div>
 </template>
 
+<script>
+import Header from "./components/Header.vue";
+
+const App = {
+  components: {
+    Header
+  },
+  name: "App",
+  props: {}
+};
+
+export default App;
+</script>
+
 <style lang="less">
+html,
+body,
+#app {
+  height: 100%;
+  margin: 0;
+}
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+  font-size: 20px;
+
+  img.rounded {
+    border-radius: 50%;
+  }
+  display: flex;
+
+  .center-children {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    @media only screen and (max-width: 768px) {
+      justify-content: center;
     }
   }
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+  }
+
+  > div,
+  > header {
+    padding: 20px;
+  }
+
+  .header {
+    flex: 0 0 auto;
+  }
+
+  .mainContent {
+    flex: 1 1 auto;
+    h1 {
+      text-align: center;
+    }
+  }
+}
+
+.content {
+  flex: 1 1 auto;
 }
 </style>
---
+++ src/views/Projects.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="content" />
+</template>
+
+<script>
+const Projects = {
+  name: "Homepage",
+  components: {}
+};
+
+export default Projects;
+</script>
+
+<style lang="less"></style>
